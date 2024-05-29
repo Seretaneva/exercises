6479138ff7a04338a12d545b939644d29af5fdd9
Seretaneva <seretan30@gmail.com>
2024-05-29 15:54:07 +0300

    Find wrong letter in a word.

---
+++ src/main/java/arrayProblems/Ortografie.java
@@ -0,0 +1,17 @@
+package arrayProblems;
+
+public class Ortografie {
+    public static void main(String[] args) {
+        printCorrectWord(1);
+    }
+
+    private static void printCorrectWord(int letter) {
+        String word = "Elvelina";
+        StringBuilder correctWord = new StringBuilder(" ");
+
+        correctWord = new StringBuilder(word.substring(0, letter) + word.substring(letter + 1, word.length()));
+        System.out.println(correctWord);
+    }
+
+}
+
