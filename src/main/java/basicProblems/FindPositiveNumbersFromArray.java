cec32563d00f4a581065b4f19497fb66d0213455
Seretaneva <seretan30@gmail.com>
2024-06-04 16:27:46 +0300

    Find positive numbers from an array and insert them in another one + find length of the new array

--- src/main/java/basicProblems/FindPositiveNumbersFromArray.java
+++ src/main/java/basicProblems/FindPositiveNumbersFromArray.java
@@ -8,25 +8,28 @@ public class FindPositiveNumbersFromArray {
         System.out.print("Enter the dimension of the array: ");
         int nr = scanner.nextInt();
         int[] array1 = new int[nr];
-
         for (int i = 0; i < nr; i++) {
             System.out.print("Enter " + (i + 1) + " number : ");
             array1[i] = scanner.nextInt();
         }
-        System.out.println("The positive numbers are :  " );
+
+        int[] array2 = new int[nr];
+        int count = 0;
         for (int i = 0; i < array1.length; i++) {
             if (array1[i] >= 0) {
-                int[] array2 = new int[]{array1[i]};
-
-                for (int j = 0; j < array2.length; j++) {
-
-                    System.out.print(array2[j] + " ");
-                }
-
-
+                array2[i] = array1[i];
+                count ++;
             }
 
         }
+        System.out.print("The dimension of the array is " + count);
+        System.out.println();
+        System.out.println("The positive numbers are :  ");
+        for (int i = 0; i < array2.length; i++) {
+            if (array2[i] != 0) {
+                System.out.print(array2[i] + " ");
 
+            }
+        }
     }
 }
