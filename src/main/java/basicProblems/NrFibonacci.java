d3fcad9b53bfa23a4f477cc1d419a11a8527accb
Seretaneva <seretan30@gmail.com>
2024-06-04 16:34:12 +0300

    Find fibonacci number

--- src/main/java/basicProblems/NrFibonacci.java
+++ src/main/java/basicProblems/NrFibonacci.java
@@ -8,23 +8,15 @@ public class NrFibonacci {
     }
 
     private static void nrFibonacci() {
-        int nr = 7;
+        int nr = 9;
         int fibonacci = 0;
         int previous = 1;
         int next = 1;
-        if (nr == 0) {
-            System.out.println(0);
-        } else if (nr == 1) {
-            System.out.println(1);
-        } else if (nr == 2) {
-            System.out.println(1);
-        } else {
-            for (int i = 3; i <= nr; i++) {
-                fibonacci = previous + next;
-                previous = next;
-                next = fibonacci;
-            }
-            System.out.println(fibonacci);
+        for (int i = 3; i <= nr; i++) {
+            fibonacci = previous + next;
+            previous = next;
+            next = fibonacci;
         }
+        System.out.println(fibonacci);
     }
 }
